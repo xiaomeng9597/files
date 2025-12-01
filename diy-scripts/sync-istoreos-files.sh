eb20458561198608ef73d1210b6c630c0920fdcc
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2025-12-01 14:50:12 +0800

    update sync-istoreos-files.sh

--- diy-scripts/sync-istoreos-files.sh
+++ diy-scripts/sync-istoreos-files.sh
@@ -9,18 +9,25 @@ cd /mnt/xiaomeng9597/files
 mfileval=$(wget -qO- "https://raw.githubusercontent.com/linkease/nas-packages/master/network/services/linkease/Makefile")
 
 # 提取版本、源URL和哈希值
-version=$(echo "$mfileval" | sed -n 's/.*PKG_SOURCE_DATE:=\([^[:space:]]*\).*/\1/p')
-if [ -z "$version" ]; then
-    version=$(echo "$mfileval" | grep -oP 'PKG_SOURCE_DATE:=\s*\K[^[:space:]]+')
+version=$(echo "$mfileval" | sed -n 's/.*PKG_SOURCE_DATE:=\([0-9.]*\).*/\1/p')
+if [[ -z "$version" ]]; then
+    version=$(echo "$mfileval" | grep -oP 'PKG_SOURCE_DATE:=\s*\K[0-9.]+')
+fi
+
+if [[ -z "$version" ]]; then
+    version=$(echo "$mfileval" | sed -n 's/.*PKG_SOURCE_DATE:=\([^[:space:]]*\).*/\1/p')
+    if [[ -z "$version" ]]; then
+        version=$(echo "$mfileval" | grep -oP 'PKG_SOURCE_DATE:=\s*\K[^[:space:]]+')
+    fi
 fi
 
 source_url=$(echo "$mfileval" | sed -n 's/.*PKG_SOURCE_URL:=\([^[:space:]]*\).*/\1/p')
-if [ -z "$source_url" ]; then
+if [[ -z "$source_url" ]]; then
     source_url=$(echo "$mfileval" | grep -oP 'PKG_SOURCE_URL:=\s*\K[^[:space:]]+')
 fi
 
 pkg_sha256=$(echo "$mfileval" | sed -n 's/.*PKG_HASH:=\([^[:space:]]*\).*/\1/p')
-if [ -z "$pkg_sha256" ]; then
+if [[ -z "$pkg_sha256" ]]; then
     pkg_sha256=$(echo "$mfileval" | grep -oP 'PKG_HASH:=\s*\K[^[:space:]]+')
 fi
 
@@ -28,11 +35,11 @@ fi
 echo "版本号：$version"
 echo "下载地址：$source_url"
 echo "文件哈希值：$pkg_sha256"
-if [ -n "$version" ] && [ -n "$source_url" ]; then
+if [[ -n "$version" ]] && [[ -n "$source_url" ]]; then
     wget --connect-timeout=1800 --read-timeout=1800 "$source_url/linkease-binary-$version.tar.gz" -O "linkease-binary-$version.tar.gz"
     # 校验下载的文件的哈希值
     downloaded_sha256=$(sha256sum "linkease-binary-$version.tar.gz" | awk '{print $1}')
-    if [ "$downloaded_sha256" != "$pkg_sha256" ]; then
+    if [[ "$downloaded_sha256" != "$pkg_sha256" ]]; then
         echo "文件哈希值不匹配，正在删除下载的文件..."
         rm "linkease-binary-$version.tar.gz"
     else
